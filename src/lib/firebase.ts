02cb33f10c96eb942a272a796c45b9514c1bfd1b
Tomáš Kebrle <tech@tomaskebrle.cz>
2021-10-25 12:49:18 +0200

    :fire: Added Firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfM40T5V1tZ33zgLzY4M229ZLv3W7j1Rg",
+  authDomain: "mesh-dev-55129.firebaseapp.com",
+  projectId: "mesh-dev-55129",
+  storageBucket: "mesh-dev-55129.appspot.com",
+  messagingSenderId: "644966883447",
+  appId: "1:644966883447:web:9d28f20e241f13faace672",
+  measurementId: "G-MF5LG7075Q"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import { getAnalytics } from 'firebase/analytics';
+	import { app } from '../lib/firebase';
+
+	// Analytics
+	const analytics = getAnalytics(app);
 </script>
 
 <slot />
