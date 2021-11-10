f73ef046ae3c2112049743f5c504c84675dfda68
Kendy205 <tech@tomaskebrle.cz>
2021-11-10 20:47:22 +0100

    :package: Made register a component instead of it begin in the register.svelte

--- src/components/ui/Button.svelte
+++ src/components/ui/Button.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	export let text;
-	export let icon;
-	export let bgColor;
-	export let textColor;
-	export let width = '72';
-	export let disabled = false;
+	export let text; // Displayed text
+	export let icon; // Icon to display from material-icons
+	export let bgColor = 'meshblue-500'; // Background color don't forget to add the color to tailwind_bundle.svelte so tailwind bundles it if it isn't already used
+	export let textColor = 'white'; // Color of the text
+	export let width = '72'; // Width default is 72
+	export let disabled = false; // Toggle if the button is disabled
 </script>
 
 <button
 	on:click
-	class="w-{width} rounded-md flex {`bg-${bgColor}`} p-2 pr-3 leading-8 align-middle text-{textColor} {disabled
+	class="w-{width} rounded-md flex items-center justify-left {`bg-${bgColor}`} p-2 pr-3 leading-8 align-middle text-{textColor} text-left {disabled
 		? 'bg-gray-400 cursor-not-allowed'
 		: ''} duration-300 cursor-pointer mb-4"
 	{disabled}
 >
-	<div class="material-icons mr-2 align-middle leading-8">
+	<div class="material-icons mr-2 ">
 		{icon}
 	</div>
 	{text}
