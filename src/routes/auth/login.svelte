44a7b7c9ae259da4beee3ceeddc24e5ba9d29226
Kendy205 <tech@tomaskebrle.cz>
2022-01-20 12:38:28 +0100

    :lipstick: Made the first half of login and register ui

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -2,4 +2,16 @@
 	import Header from '$lib/components/ui/Header.svelte';
 </script>
 
-<Header>Login</Header>
+<section class="lg:flex w-screen h-screen">
+	<section class="lg:w-1/2 w-full h-screen p-6" style="background-image: url(/background.png);">
+		<h1 class="text-white mb-4">
+			Login to <br />
+			school.mesh
+		</h1>
+		<a href="/auth/register">
+			<h4 class="text-meshblue-300 hover:underline max-w-max">
+				Dont have an account? Create one here!
+			</h4>
+		</a>
+	</section>
+</section>
