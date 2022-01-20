08090ffe01b5ab3c58512b72236f6fb37b44f6f2
Kendy205 <tech@tomaskebrle.cz>
2022-01-20 16:34:06 +0100

    :lipstick: Improved input

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -6,13 +6,14 @@
 	export let id;
 	export let error;
 	export let type = 'text';
+	export let note = ' ';
 	let input;
 	onMount(() => {
 		input.type = type;
 	});
 </script>
 
-<div class="relative my-4 mx-2 w-full">
+<div class="relative my-2 mx-2">
 	<input
 		type="text"
 		bind:value
@@ -20,14 +21,17 @@
 		placeholder="something"
 		{id}
 		name="id"
-		class="w-full h-10 rounded-md border-2 border-{error
+		class="w-full h-10 border-b-2 border-0 p-0 border-b-{error
 			? 'red-400'
 			: 'gray-600'} focus:border-meshblue-500 focus:outline-none focus:ring-0 peer placeholder-transparent"
 	/>
 	<label
 		for={id}
-		class="absolute left-3 -top-2.5 px-1 bg-white text-sm text-gray-600 transition-all duration-300 pointer-events-none
-     peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-gray-600 peer-focus:text-sm peer-focus:left-3 peer-placeholder-shown:left-3
+		class="absolute left-0 -top-2.5 bg-white text-sm text-gray-600 transition-all duration-300 pointer-events-none
+     peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-gray-600 peer-focus:text-sm
   ">{placeholder}</label
 	>
+	<div class="text-gray-600">
+		{note}
+	</div>
 </div>
--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -1,17 +1,30 @@
 <script>
-	import Header from '$lib/components/ui/Header.svelte';
+	import Input from '$lib/components/ui/Input.svelte';
+
+	// Variables to bind to
+	let name;
 </script>
 
 <section class="lg:flex w-screen h-screen">
-	<section class="lg:w-1/2 w-full h-screen p-6" style="background-image: url(/background.png);">
-		<h1 class="text-white mb-4">
+	<section class="lg:w-1/3 h-screen pl-6 pt-6" style="background-image: url(/background.png);">
+		<h1 class="text-white mb-4 overflow-hidden max-w-max">
 			Login to <br />
-			school.mesh
+			School Mesh
 		</h1>
 		<a href="/auth/register">
-			<h4 class="text-meshblue-300 hover:underline max-w-max">
+			<h4 class="text-meshblue-300 hover:border-b-2 border-meshblue-300 max-w-max">
 				Dont have an account? Create one here!
 			</h4>
 		</a>
+	</section>
+
+	<section class="grid place-items-center lg:w-2/3 w-full">
+		<div class="rounded-md shadow-md p-6 bg-white relative">
+			<div class="flex">
+				<Input bind:value={name} placeholder="First Name" id="first_name" error={false} />
+				<Input bind:value={name} placeholder="First Name" id="first_name" error={false} />
+			</div>
+			<Input bind:value={name} placeholder="First Name" id="first_name" error={false} />
+		</div>
 	</section>
 </section>
