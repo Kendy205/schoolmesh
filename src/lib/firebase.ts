a1adb43e21ecd214dc70f2d4ba894a467c2939e7
Kendy205 <tech@tomaskebrle.cz>
2022-02-05 16:37:40 +0100

    :fire: Entire rewrite

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,58 +1,42 @@
-import {
-  initializeApp
-} from "@firebase/app";
-import {
-  getFirestore, doc, writeBatch, Firestore
-} from "@firebase/firestore"
-import {
-  getAuth,
-  GoogleAuthProvider,
-  FacebookAuthProvider,
-  GithubAuthProvider,
-  OAuthProvider
-} from "@firebase/auth"
-import { getFunctions } from "@firebase/functions"
+import { initializeApp } from '@firebase/app';
+import { getFirestore, doc, writeBatch } from '@firebase/firestore';
+import { getAuth } from '@firebase/auth';
+import { getFunctions } from '@firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfM40T5V1tZ33zgLzY4M229ZLv3W7j1Rg",
-  authDomain: "mesh-dev-55129.firebaseapp.com",
-  projectId: "mesh-dev-55129",
-  storageBucket: "mesh-dev-55129.appspot.com",
-  messagingSenderId: "644966883447",
-  appId: "1:644966883447:web:9d28f20e241f13faace672",
-  measurementId: "G-MF5LG7075Q"
+	apiKey: 'AIzaSyAfM40T5V1tZ33zgLzY4M229ZLv3W7j1Rg',
+	authDomain: 'mesh-dev-55129.firebaseapp.com',
+	projectId: 'mesh-dev-55129',
+	storageBucket: 'mesh-dev-55129.appspot.com',
+	messagingSenderId: '644966883447',
+	appId: '1:644966883447:web:9d28f20e241f13faace672',
+	measurementId: 'G-MF5LG7075Q'
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-
 //Exports
 export const db = getFirestore();
-export const auth = getAuth()
-export const functions = getFunctions(app, "europe-west1")
-
-// Auth Providers
-export const googleAuth = new GoogleAuthProvider()
-export const facebookAuth = new FacebookAuthProvider()
-export const microsoftAuth = new OAuthProvider("microsoft.com")
+export const auth = getAuth();
+export const functions = getFunctions(app, 'europe-west1');
 
 export const storeUserData = async (db, user, username, displayName) => {
-  console.log({user,username})
+	console.log({ user, username });
 
-  const batch = writeBatch(db)
+	const batch = writeBatch(db);
 
-  batch.set(doc(db,"usernames", username),{
-    uid : user.uid
-  })
+	batch.set(doc(db, 'usernames', username), {
+		uid: user.uid
+	});
 
-  batch.set(doc(db, "users", user.uid), {
-    displayName: displayName,
-    photoURL: "/profile_picture",
-    username: username,
-    uid: user.uid
-  })
+	batch.set(doc(db, 'users', user.uid), {
+		displayName: displayName,
+		photoURL: '/profile_picture',
+		username: username,
+		uid: user.uid
+	});
 
-  await batch.commit()
-  return
-}
+	await batch.commit();
+	return;
+};
